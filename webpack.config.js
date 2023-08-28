1a86285e0c1b45e3e8b9b8bc883303067b0d32f5
dayu <dayu@liveio.cn>
2023-08-28 20:52:40 +0800

    增加配置

--- webpack.config.js
+++ webpack.config.js
@@ -225,18 +225,22 @@ module.exports = {
     }),
 
     // 复制public文件
-    // ...(isProd && fs.statSync(resolve("./public")).isDirectory()
-    //   ? [
-    //       new CopyWebpackPlugin({
-    //         patterns: [
-    //           {
-    //             from: resolve("./public"),
-    //             to: resolve("./dist"),
-    //           },
-    //         ],
-    //       }),
-    //     ]
-    //   : []),
+    ...(isProd
+      ? [
+          new CopyWebpackPlugin({
+            patterns: [
+              {
+                from: resolve("./src/templates"),
+                to: resolve("./dist/templates"),
+              },
+              {
+                from: resolve("./daobox-theme.yaml"),
+                to: resolve("./dist/"),
+              },
+            ],
+          }),
+        ]
+      : []),
 
     ...(process.env.PROFILE === '1' ? [new BundleAnalyzerPlugin()] : []),
 
